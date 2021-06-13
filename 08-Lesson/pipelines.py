4edcb46ed25ee96b01528e45910c8ad98e465e95
Yakov-Jak <76076258+Yakov-Jak@users.noreply.github.com>
2021-06-14 00:07:42 +0300

    Add files via upload

---
+++ 08-Lesson/pipelines.py
@@ -0,0 +1,27 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+from pymongo import MongoClient
+
+
+class InstaparsPipeline:
+    def __init__(self):
+        client = MongoClient('127.0.0.1', 27017)
+        client.drop_database('insta_db')
+        self.mongobase = client.insta_db
+
+
+    def process_item(self, item, spider):
+        print()
+
+
+        collection = self.mongobase[item['username']]
+        collection.insert_one(item)
+
+
+        return item
